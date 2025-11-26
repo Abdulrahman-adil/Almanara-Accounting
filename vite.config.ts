426d2c5510836ca6f48e6d6fe33c31741e1d3ac8
Abdulrahman-adil <abdo48439@email.com>
2025-11-26 13:26:09 +0400

    Setup vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, '.', '')
 
   return {
     // 👈 مهم جدًا علشان GitHub Pages
     // غيّر الاسم هنا لو الريبو هيتسمّي حاجة تانية
-    base: '/Almanara-Accounting/',
+    base: '/',
 
     server: {
       port: 3000,
@@ -27,5 +27,5 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, '.'),
       },
     },
-  };
-});
+  }
+})
